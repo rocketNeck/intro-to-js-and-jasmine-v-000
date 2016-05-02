639e8e37cce1ca392aeaf6bd68e0636e9f733ac8
rocketNeck <homestarflyer@gmail.com>
2016-05-02 15:44:34 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,19 @@
 'use strict';
+
+
+function favIceCream(icecream) {
+  return "I love " + icecream;
+}
+
+
+function shouting(st) {
+  return st.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth() {
+  return true;
+}
